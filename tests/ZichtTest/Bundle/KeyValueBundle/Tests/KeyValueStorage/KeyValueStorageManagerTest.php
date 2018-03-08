4d6b037ab330b14255a7254644009ef0be694d9d
Boudewijn Schoon <boudewijn@zicht.nl>
2018-03-08 15:41:36 +0100

    [ZICHTDEV-731] Add locale dependent unit test

--- tests/ZichtTest/Bundle/KeyValueBundle/Tests/KeyValueStorage/KeyValueStorageManagerTest.php
+++ tests/ZichtTest/Bundle/KeyValueBundle/Tests/KeyValueStorage/KeyValueStorageManagerTest.php
@@ -9,7 +9,10 @@ use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Zicht\Bundle\KeyValueBundle\Entity\KeyValueStorage;
+use Zicht\Bundle\KeyValueBundle\KeyValueStorage\Exception\KeyAlreadyExistsException;
+use Zicht\Bundle\KeyValueBundle\KeyValueStorage\Exception\KeyNotFoundException;
 use Zicht\Bundle\KeyValueBundle\KeyValueStorage\KeyValueStorageManager;
+use Zicht\Bundle\KeyValueBundle\KeyValueStorage\LocaleDependentData;
 use ZichtTest\Bundle\KeyValueBundle\Tests\KeyValueStorage\FooKeysDefiner;
 
 require 'FooKeysDefiner.php';
@@ -19,9 +22,20 @@ require 'FooKeysDefiner.php';
  */
 class KeyValueStorageManagerTest extends WebTestCase
 {
+    /**
+     * {@inheritdoc}
+     */
+    public static function setUpBeforeClass()
+    {
+        // 'en' locale as default
+        LocaleDependentData::setLocale('zz');
+    }
 
     /**
      * Test adding KeysDefiner results in keys existing with the right value.
+     *
+     * @throws KeyAlreadyExistsException
+     * @throws KeyNotFoundException
      */
     public function testAddingKeyDefiners()
     {
@@ -33,9 +47,10 @@ class KeyValueStorageManagerTest extends WebTestCase
         $definer = new FooKeysDefiner();
         $manager->addKeysDefiner($definer);
 
-        $this->assertEquals(['foo-key', 'bar-key'], $manager->getAllKeys());
+        $this->assertEquals(['foo-key', 'bar-key', 'test-locale'], $manager->getAllKeys());
         $this->assertEquals('foo-value', $manager->getValue('foo-key'));
         $this->assertEquals('bar-value', $manager->getValue('bar-key'));
+        $this->assertEquals('Garble garble', $manager->getValue('test-locale'));
     }
 
     /**
@@ -53,6 +68,8 @@ class KeyValueStorageManagerTest extends WebTestCase
 
     /**
      * Test keys that are missing from the DB are marked.
+     *
+     * @throws KeyAlreadyExistsException
      */
     public function testMissingDBKeys()
     {
@@ -65,11 +82,14 @@ class KeyValueStorageManagerTest extends WebTestCase
         $definer = new FooKeysDefiner();
         $manager->addKeysDefiner($definer);
 
-        $this->assertEquals(['foo-key', 'bar-key'], $manager->getMissingDBKeys());
+        $this->assertEquals(['foo-key', 'bar-key', 'test-locale'], $manager->getMissingDBKeys());
     }
 
     /**
      * Test that values in the DB are returned and not the default values.
+     *
+     * @throws KeyAlreadyExistsException
+     * @throws KeyNotFoundException
      */
     public function testValuesFromDB()
     {
@@ -81,20 +101,29 @@ class KeyValueStorageManagerTest extends WebTestCase
         $entity2->setStorageKey('bar-key');
         $entity2->setStorageValue(strrev('bar-value'));
 
+        $entity3 = new KeyValueStorage();
+        $entity3->setStorageKey('test-locale');
+        $entity3->setStorageValue(['nl' => strrev('Hallo Wereld'), 'zz' => strrev('Garble garble'), 'en' => strrev('Hello World')]);
+
         $em = $this->getEntityManager();
         $repo = $this->getRepository();
         $repo->expects(self::at(0))->method('findOneBy')->with(['storageKey' => 'foo-key'])->willReturn($entity);
         $repo->expects(self::at(1))->method('findOneBy')->with(['storageKey' => 'bar-key'])->willReturn($entity2);
+        $repo->expects(self::at(2))->method('findOneBy')->with(['storageKey' => 'test-locale'])->willReturn($entity3);
         $em->method('getRepository')->willReturn($repo);
 
         $manager = new KeyValueStorageManager($em, '/tmp/web', '/tmp/web/media/key_value_storage');
         $manager->addKeysDefiner(new FooKeysDefiner());
         $this->assertEquals(strrev('foo-value'), $manager->getValue('foo-key'));
         $this->assertEquals(strrev('bar-value'), $manager->getValue('bar-key'));
+        $this->assertEquals(strrev('Garble garble'), $manager->getValue('test-locale'));
     }
 
     /**
      * Test with a mix of predefined values and DB-values.
+     *
+     * @throws KeyAlreadyExistsException
+     * @throws KeyNotFoundException
      */
     public function testMixedValues()
     {
