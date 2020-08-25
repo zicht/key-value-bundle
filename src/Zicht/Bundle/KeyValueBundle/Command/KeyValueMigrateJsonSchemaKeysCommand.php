bb1d041daff58e3800e71812775d323825ee8dc0
Boudewijn Schoon <boudewijn@zicht.nl>
2020-08-25 12:07:57 +0200

    Support migration option `--replace-invalid`

--- src/Zicht/Bundle/KeyValueBundle/Command/KeyValueMigrateJsonSchemaKeysCommand.php
+++ src/Zicht/Bundle/KeyValueBundle/Command/KeyValueMigrateJsonSchemaKeysCommand.php
@@ -11,6 +11,7 @@ use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Zicht\Bundle\KeyValueBundle\KeyValueStorage\KeyValueStorageManager;
 use Zicht\Bundle\KeyValueBundle\KeyValueStorage\PredefinedJsonSchemaKey;
 
@@ -24,6 +25,7 @@ class KeyValueMigrateJsonSchemaKeysCommand extends ContainerAwareCommand
         $this
             ->setName('zicht:key-value:migrate-json-schema-keys')
             ->setDescription('Validate all json schema keys and migrate when possible')
+            ->addOption('replace-invalid', null, InputOption::VALUE_NONE, 'Replace stored value with default value when unable to migrate')
             ->addOption('force', null, InputOption::VALUE_NONE, 'Force migration in the database to occur');
     }
 
@@ -32,6 +34,8 @@ class KeyValueMigrateJsonSchemaKeysCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $io = new SymfonyStyle($input, $output);
+
         $table = new Table($output);
         $table->setHeaders(['Key', 'Default', 'Storage', 'Migrate']);
 
@@ -46,12 +50,16 @@ class KeyValueMigrateJsonSchemaKeysCommand extends ContainerAwareCommand
                 $key = $predefinedKey->getKey();
                 $defaultValue = $predefinedKey->getValue();
                 if (!$defaultValueIsValid = $predefinedKey->isValid($defaultValue, $message)) {
-                    $output->writeln(sprintf('<error>%s</error> - %s - validation error on default value', $message, $key));
+                    $io->writeln(sprintf('%s - validation error on default value', $key));
+                    $io->writeln($message);
+                    $io->error(json_encode($defaultValue, JSON_PRETTY_PRINT));
                 }
                 $defaultState = $defaultValueIsValid ? 'ok' : '<error>invalid</error>';
                 $storageValue = $storageManager->getValue($key);
-                if(!$storageValueIsValid = $predefinedKey->isValid($storageValue, $message)) {
-                    $output->writeln(sprintf('<error>%s</error> - %s - validation error on storage value', $message, $key));
+                if (!$storageValueIsValid = $predefinedKey->isValid($storageValue, $message)) {
+                    $io->writeln(sprintf('%s - validation error on storage value', $key));
+                    $io->writeln($message);
+                    $io->error(json_encode($storageValue, JSON_PRETTY_PRINT));
                 }
                 $storageState = $storageValueIsValid ? 'ok' : '<error>invalid</error>';
                 $migrateState = $storageValueIsValid ? '' : '<error>unable to migrate</error>';
@@ -59,7 +67,9 @@ class KeyValueMigrateJsonSchemaKeysCommand extends ContainerAwareCommand
                 if ($defaultValueIsValid && !$storageValueIsValid) {
                     $migrateValue = $predefinedKey->migrate($storageValue);
                     if (!$migrateValueIsValid = $predefinedKey->isValid($migrateValue, $message)) {
-                        $output->writeln(sprintf('<error>%s</error> - %s - validation error on migrated value', $message, $key));
+                        $io->writeln(sprintf('%s - validation error on migrated value', $key));
+                        $io->writeln($message);
+                        $io->error(json_encode($migrateValue, JSON_PRETTY_PRINT));
                     }
 
                     if ($migrateValueIsValid) {
@@ -70,6 +80,17 @@ class KeyValueMigrateJsonSchemaKeysCommand extends ContainerAwareCommand
                             $migrateState = 'migrated';
                         } else {
                             $migrateState = 'use --force to migrate';
+                        }
+                    } else {
+                        if ($input->getOption('replace-invalid')) {
+                            if ($input->getOption('force')) {
+                                $storageManager->saveValue($key, $defaultValue);
+                                $doctrine->getEntityManager()->flush();
+                                $storageState = strip_tags($storageState);
+                                $migrateState = 'replaced with default';
+                            } else {
+                                $migrateState = 'use --force to replace with default';
+                            }
                         }
                     }
                 }
